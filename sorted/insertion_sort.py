2e4a174a0b71cd33e107dbe09fb6809835936016
Стас Балджи <stanislavbaldzhy@yandex.ru>
2022-11-27 23:05:40 +0300

    Реализация алгоритма сортировки вставками

---
+++ sorted/insertion_sort.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+def insertion_sort(array: List[int]) -> None:
+    '''Получает на вход неотсортированный список. 
+    При переборе каждого элемента выстраивает их в порядке возростания,
+    перемещая поэлементно вправо, освобождая место для вставки элемента.
+    Каждую итерацию выводит на печать.
+    '''
+
+    for i in range(len(array)):
+        item_to_insert = array[i]
+        j = i
+        while j > 0 and item_to_insert < array[j - 1]:
+            array[j] = array[j - 1]
+            j -= 1
+        array[j] = item_to_insert
+        print(f'step: {i}, sorted: {i + 1}, elements: {array}')
+
+
+insertion_sort([11, 2, 9, 7, 1])
